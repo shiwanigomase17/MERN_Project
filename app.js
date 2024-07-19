c1f49002feb86800022f64e92a410640dee029ae
shiwanigomase17 <shiwanigomase17@gmail.com>
2024-07-20 02:15:50 +0530

    Error Handling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -54,12 +55,13 @@ app.get("/listings/:id", async(req, res) => {
 });
 
 //Create Route
-app.post("/listings",async(req, res) => {
-    //let{title, description, image, price, location, country}= req.body;
-    const newListing = new Listing(req.body.listing);
-    await newListing.save()
-     res.redirect("/listings");
-});
+app.post("/listings",wrapAsync(async(req, res, next) => {
+    
+        const newListing = new Listing(req.body.listing);
+        await newListing.save()
+         res.redirect("/listings");
+    
+}));
 
 //Edit Route
 app.get("/listings/:id/edit", async(req, res) =>{
@@ -100,6 +102,10 @@ app.delete("/listings/:id", async(req, res)=>{
 //     console.log("sample was saved");
 //     res.send("successful");
 // });
+
+app.use((err, req, res, nex) =>{
+    res.send("Something went wrong!");
+});
 
 app.listen(8080, () =>{
     console.log("server listening on port 8080");
--- models/listing.js
+++ models/listing.js
@@ -4,20 +4,21 @@ const Schema = mongoose.Schema;
 const listingSchema = new Schema({
     title :{
         type: String,
-        require:true,
+        required:true,
         } ,
     description: String,
     image: {
 
         filename : String,
-        url: String,
-        // type: String,
-        // default: "https://images.unsplash.com/photo-1529348915581-73628f0cf212?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        // set : (v) =>
-        //      v=== " " 
-        //        ? "https://images.unsplash.com/photo-1529348915581-73628f0cf212?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
-        //        : v,
-    },
+        url: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1529348915581-73628f0cf212?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set : (v) =>
+             v=== " " 
+               ? "https://images.unsplash.com/photo-1529348915581-73628f0cf212?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+               : v,
+       },
+   },
     price: Number,
     location: String,
     country: String,
