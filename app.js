f9dcc680368d482cf90636a5ce093ef3bd9f9f39
shiwanigomase17 <shiwanigomase17@gmail.com>
2024-07-28 18:21:36 +0530

    middleware added

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -28,83 +33,44 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const sessionOptions = {
+    secret : "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge :7 * 24 * 60 * 60 * 1000,
+        httpOnly : true
+    },
+};
 
 app.get("/" , (req , res) => {
     res.send("hii i am root");
 });
 
-// Index Route
-app.get("/listings", async(req, res) => {
-    const allListings= await Listing.find({});
-    res.render("listings/index.ejs", {allListings});
-   
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 });
 
 
-//New route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
 
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);
+
+
+app.all("*", (req, res, next) =>{
+    next(new ExpressError(404, "Page not found!"));
 });
-
-//Show Route
-app.get("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
-});
-
-//Create Route
-app.post("/listings",wrapAsync(async(req, res, next) => {
-    
-        const newListing = new Listing(req.body.listing);
-        await newListing.save()
-         res.redirect("/listings");
-    
-}));
-
-//Edit Route
-app.get("/listings/:id/edit", async(req, res) =>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", {listing});
-});
-
-//Update Route
-app.put("/listings/:id", async(req, res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-     res.redirect(`/listings/${id}`);
-});
-
-//Delete Route
-app.delete("/listings/:id", async(req, res)=>{
-    let{id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-
-});
-    
-   
-
-
-// app.get("/testListing", async(req, res) => {
-//     let sampleListing = new Listing({
-//         title : "My new vill",
-//         description: "By the beach",
-//         price : 1200,
-//         location: "Calangute, goa",
-//         country: "India",
-
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("successful");
-// });
 
 app.use((err, req, res, nex) =>{
-    res.send("Something went wrong!");
+    let{statusCode = 500, message = "Something went wrong!"} = err;
+     res.status (statusCode).render("error.ejs", {message});
+    //res.status(statusCode).send(message);
 });
 
 app.listen(8080, () =>{
